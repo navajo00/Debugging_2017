f8b52b323a38ed6638b79d25b272925a7a1c695d
Christopher <christopher.navarro@acu.edu.au>
2017-10-12 21:32:57 +1100

    T002

--- Game/src/sourceCode/Game.java
+++ Game/src/sourceCode/Game.java
@@ -30,7 +30,7 @@ public class Game {
 		if (pick == null) throw new IllegalArgumentException("Pick cannot be negative.");
 		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
 		
-		player.takeBet(bet);
+		//player.takeBet(bet);
 		    
 		int matches = 0;
 		for ( Dice d : dice) {
@@ -44,6 +44,9 @@ public class Game {
 
 		if (matches > 0) {			
 			player.receiveWinnings(winnings);
+		}
+		else {
+			player.takeBet(bet);
 		}
         return winnings;		
 	}
